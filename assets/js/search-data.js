3dfffb717ba93b8c3816de02c072486e69338bd2
chidavcheng <116243219+chidavcheng@users.noreply.github.com>
2026-05-10 08:11:31 +0000

    Deploying to gh-pages from @ chidavcheng/chi-lab@795b2a6b34c54e98bcf6b66887aa21f4d97626a7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "publications in reversed chronological order.",
+          description: "in reversed chronological order.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/chi-lab/publications/";
