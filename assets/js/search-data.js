3438f95f482ddea41c868d78aa8ff038b3694418
chidavcheng <116243219+chidavcheng@users.noreply.github.com>
2026-05-10 08:08:35 +0000

    Deploying to gh-pages from @ chidavcheng/chi-lab@96ca2b3afd8d3863445f91b1267721417f0a60b6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/chi-lab/publications/";
           },
+        },{id: "nav-people",
+          title: "People",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/chi-lab/people/";
+          },
         },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
@@ -29,13 +36,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/chi-lab/teaching/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/chi-lab/people/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
