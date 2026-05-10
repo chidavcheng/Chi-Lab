9f29578e9c4bd063d824b8f98892587649864a59
chidavcheng <116243219+chidavcheng@users.noreply.github.com>
2026-05-10 07:11:58 +0000

    Deploying to gh-pages from @ chidavcheng/chi-lab@e730382809c140468fc1bb4116b3a3df938fcf21 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -507,46 +507,25 @@ ninja.data = [{
           section: "Teachings",handler: () => {
               window.location.href = "/chi-lab/teachings/introduction-to-machine-learning/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/chi-lab/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/chi-lab/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/chi-david-cheng-ph-d-a116b743", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=P_d6A68AAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://orcid.org/0000-0001-8596-1801", "_blank");
         },
       },{
       id: 'light-theme',
