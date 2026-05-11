ab38ad48ed19bc60ba308c4fff9617bc8264f778
chidavcheng <116243219+chidavcheng@users.noreply.github.com>
2026-05-11 04:26:03 +0000

    Deploying to gh-pages from @ chidavcheng/chi-lab@f3ed767933a19612cd32d0a3ad2c16efea905c3a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "Members of the Chi Lab.",
+          description: "Members of the Chi Lab at UNSW Sydney.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/chi-lab/people/";
