3c853044f4520185c7d1fa05fce21a970ff60ec1
chidavcheng <116243219+chidavcheng@users.noreply.github.com>
2026-05-12 10:40:20 +0000

    Deploying to gh-pages from @ chidavcheng/chi-lab@9bcc2246602710b4793f7fa5376b161a26bcb29a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-chi-d-cheng",
-    title: "Chi D. Cheng",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/chi-lab/";
