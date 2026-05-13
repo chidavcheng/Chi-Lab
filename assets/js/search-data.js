3181bd0e151308f615b9978a30009ac40f87f322
chidavcheng <116243219+chidavcheng@users.noreply.github.com>
2026-05-13 11:04:19 +0000

    Deploying to gh-pages from @ chidavcheng/chi-lab@9df5f25515c62a02c3fd4cdbc4ac02f298f89fa7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -98,17 +98,7 @@ ninja.data = [{
           section: "News",},{id: "news-zhouhao-was-awarded-the-2026-hdr-development-and-research-training-grant-drtg-congratulations",
           title: 'Zhouhao was awarded the 2026 HDR Development and Research Training Grant (DRTG). Congratulations!...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/chi-lab/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/chi-lab/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
+          section: "News",},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
